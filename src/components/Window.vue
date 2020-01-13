bf3c4107e08ab19e40ff0f95f2c27bb3f1b6003e
Stasea <vovod9999@bk.ru>
2020-01-13 20:51:56 +0200

    add textarea

--- src/components/Window.vue
+++ src/components/Window.vue
@@ -1,41 +1,75 @@
 <script>
+  const fs = require("fs");
+  import db from "../facades/db"
 
   export default {
     name: 'Window',
     data() {
       return {
-        name: 'puişor',
-        text: ''
+        dataBd: '',
+        data: '',
+        text: '',
+        first_name: '',
+        second_name: '',
+        users: []
       }
     },
     mounted() {
     },
     methods: {
-      send() {
-        console.log('Privet')
+      readText() {
+        this.data = fs.readFileSync("./src/files/data.txt", "utf8");
+      },
+      writeText() {
+        fs.writeFile("./src/files/data.txt", this.text, (error) => {
+          if (error) throw error; // если возникла ошибка
+        });
+      },
+      async readBd() {
+        this.users = await db.query(`SELECT * FROM users`)
+      },
+      writeBd() {
+        db.query(`INSERT INTO users (first_name, second_name)
+        VALUES ('${this.first_name}', '${this.second_name}');`)
+      },
+      readBdinFile() {
+        this.dataBd = JSON.parse(fs.readFileSync("./src/files/dataBd.txt", "utf8"));
+        console.log(this.dataBd);
+      },
+      writeBdinFile() {
+        fs.writeFile("./src/files/dataBd.txt", JSON.stringify(this.users), (error) => {
+          if (error) throw error; // если возникла ошибка
+        });
       }
-    }
+    },
+
   };
 </script>
 
 <template>
   <div>
-    <div>Privet {{name}}!</div>
-    <div>{{text}}!</div>
-    <v-text-field
-      class="mt-3"
-      v-model="name"
-      label="Outlined"
-      placeholder="Placeholder"
-      outlined
-    />
     <v-textarea
       v-model="text"
       solo
       name="input-7-4"
       label="Solo textarea"
     />
-    <v-btn text small color="error">Error</v-btn>
+    <v-btn @click="readText()" small color="green">read</v-btn>
+    <v-btn @click="writeText()" small color="blue">write</v-btn>
+    <v-text-field
+      v-model="first_name"
+      label="first name"
+    />
+    <v-text-field
+      v-model="second_name"
+      label="second name"
+    />
+    <v-btn @click="readBd()" small color="green">readBD</v-btn>
+    <v-btn @click="writeBd()" small color="blue">writeBD</v-btn>
+    <br>
+    <v-btn @click="readBdinFile()" small color="pink">readBDinFile</v-btn>
+    <v-btn @click="writeBdinFile()" small color="aquamarine">writeBDinFile</v-btn>
+    <div v-for="item in users">{{item.first_name}} {{item.second_name}}</div>
   </div>
 
 </template>
